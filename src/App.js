4da54da1d13a3b8d613171fa3b59f69392881dd3
Rushikesh <pitalerushikesh@gmail.com>
2022-03-16 12:29:41 +0530

    one with ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
-import { Card } from "@mui/material";
+import { Avatar, Card, CardContent, Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import "./App.css";
+import data from "./data/birthday.json";
+
+import React from "react";
+
+const BirthCard = ({ name, age, number }) => {
+  return (
+    <Card elevation={0}>
+      <CardContent>
+        <Grid container>
+          <Grid item xs={4}>
+            <Avatar
+              alt="Remy Sharp"
+              src={`https://i.pravatar.cc/150?img=${number}`}
+              sx={{
+                width: 100,
+                height: 100,
+                boxShadow: "0 5px 10px grey",
+              }}
+            />
+          </Grid>
+          <Grid item xs={8} alignItems="center" display="flex">
+            <Box>
+              <Typography variant="h5" fontWeight="bold">
+                {name}
+              </Typography>
+              <Typography variany="subtitle1">{age} years</Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
 
 function App() {
+  const birthData = data.Birthdays;
   return (
     <div className="App">
-      <Box>
-        <Card>
-          <h1>Hello World</h1>
-        </Card>
+      <Box
+        sx={{
+          backgroundColor: "#fff",
+          p: 3,
+          width: "100%",
+        }}
+      >
+        <Typography variant="h3">{birthData.length} birthdays today</Typography>
+        {birthData.map((person, index) => {
+          return (
+            <BirthCard
+              key={index}
+              name={person.name}
+              age={person.age}
+              number={index}
+            />
+          );
+        })}
       </Box>
     </div>
   );
