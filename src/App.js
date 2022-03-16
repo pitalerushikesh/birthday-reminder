07eafc2e8d7c6538e027fc9a649f9c502bd009bd
Rushikesh <pitalerushikesh@gmail.com>
2022-03-16 13:54:56 +0530

    final done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,49 @@
-import { Avatar, Card, CardContent, Grid, Typography } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import "./App.css";
 import data from "./data/birthday.json";
-
-import React from "react";
+import React, { useState } from "react";
 
 const BirthCard = ({ name, age, number }) => {
   return (
     <Card elevation={0}>
-      <CardContent>
-        <Grid container>
-          <Grid item xs={4}>
+      <CardContent
+        sx={{
+          p: 0,
+        }}
+      >
+        <Grid container spacing={11}>
+          <Grid item xs={2}>
             <Avatar
               alt="Remy Sharp"
               src={`https://i.pravatar.cc/150?img=${number}`}
               sx={{
-                width: 100,
-                height: 100,
+                width: 80,
+                height: 80,
                 boxShadow: "0 5px 10px grey",
               }}
             />
           </Grid>
-          <Grid item xs={8} alignItems="center" display="flex">
+          <Grid item xs={10} alignItems="center" display="flex">
             <Box>
-              <Typography variant="h5" fontWeight="bold">
+              <Typography variant="h6" fontWeight="bold">
                 {name}
               </Typography>
-              <Typography variany="subtitle1">{age} years</Typography>
+              <Typography
+                variany="subtitle1"
+                sx={{
+                  color: "#707070",
+                }}
+              >
+                {age} years
+              </Typography>
             </Box>
           </Grid>
         </Grid>
@@ -37,17 +54,35 @@ const BirthCard = ({ name, age, number }) => {
 
 function App() {
   const birthData = data.Birthdays;
+
+  const [birthdays, setBirthdays] = useState(birthData);
+
+  const onDelete = (data) => {
+    console.log(data);
+    setBirthdays([]);
+  };
+
   return (
     <div className="App">
       <Box
         sx={{
           backgroundColor: "#fff",
           p: 3,
-          width: "100%",
+          minWidth: 450,
+          boxShadow: "0 5px 10px grey",
+          borderRadius: 2,
         }}
       >
-        <Typography variant="h3">{birthData.length} birthdays today</Typography>
-        {birthData.map((person, index) => {
+        <Typography
+          fontFamily="Ubuntu"
+          sx={{
+            pb: 2,
+            fontSize: "1.8rem",
+          }}
+        >
+          {birthdays.length} birthdays today
+        </Typography>
+        {birthdays.map((person, index) => {
           return (
             <BirthCard
               key={index}
@@ -57,6 +92,24 @@ function App() {
             />
           );
         })}
+        <Button
+          variant="contained"
+          sx={{
+            width: "100%",
+            textTransform: "none",
+            fontWeight: "bold",
+            fontSize: "1.2rem",
+            backgroundColor: "#f28ab2",
+            "&:hover": {
+              backgroundColor: "#f28ab2",
+            },
+          }}
+          onClick={() => {
+            onDelete(birthdays);
+          }}
+        >
+          Clear All
+        </Button>
       </Box>
     </div>
   );
