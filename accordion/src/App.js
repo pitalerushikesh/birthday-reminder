616dcdd31d39d7fbf44af44fd08d667d8eda7d84
Rushikesh <pitalerushikesh@gmail.com>
2022-03-19 13:22:58 +0530

    done

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,7 +1,49 @@
 import "./App.css";
+import MuiAccordion from "./components/Accordion";
+import { Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import data from "./data/data.json";
 
 function App() {
-  return <div className="App"></div>;
+  const queryData = data.queries;
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#ffffff",
+        width: "650px",
+        p: 4,
+      }}
+    >
+      <Grid container spacing={4}>
+        <Grid
+          item
+          xs={4}
+          justifyContent="center"
+          alignItems="start"
+          display="flex"
+        >
+          <Typography fontSize="1.5rem" fontFamily="Ubuntu">
+            Questions And Answers About Login
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={8}
+          justifyContent="center"
+          alignItems="center"
+          display="flex"
+          flexDirection="column"
+        >
+          {queryData.map((query, index) => {
+            return (
+              <MuiAccordion ques={query.ques} ans={query.ans} key={index} />
+            );
+          })}
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
 export default App;
---
+++ accordion/src/components/Accordion.js
@@ -0,0 +1,49 @@
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+
+const MuiAccordion = ({ ques, ans }) => {
+  const [expanded, setExpanded] = useState(false);
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <>
+      <Accordion
+        elevation={2}
+        sx={{
+          mb: 2,
+        }}
+        expanded={expanded === "panel1"}
+        onChange={handleChange("panel1")}
+      >
+        <AccordionSummary
+          expandIcon={
+            expanded ? (
+              <AiOutlineMinus color="#b4345c" />
+            ) : (
+              <AiOutlinePlus color="#b4345c" />
+            )
+          }
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography fontWeight="bold" fontFamily="Roboto">
+            {ques}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>{ans}</Typography>
+        </AccordionDetails>
+      </Accordion>
+    </>
+  );
+};
+
+export default MuiAccordion;
