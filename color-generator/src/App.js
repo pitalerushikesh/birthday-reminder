c024bbb92abc1d11b8295af16793025defcf6cc1
Rushikesh <pitalerushikesh@gmail.com>
2022-03-28 16:34:42 +0530

    working on ui

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -1,7 +1,42 @@
+import React from "react";
 import "./App.css";
+import { Box, Button, FormLabel, TextField } from "@mui/material";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Box display="flex" justifyContent="space-evenly">
+        <FormLabel
+          sx={{
+            fontSize: "1.25rem",
+            color: "#063251",
+            letterSpacing: "0.1rem",
+          }}
+        >
+          Color Generator
+        </FormLabel>
+        <TextField
+          sx={{
+            mx: 2,
+          }}
+          type="number"
+          size="small"
+          autoComplete="off"
+          placeholder="#f15025"
+        />
+        <Button
+          sx={{
+            backgroundColor: "#49a6e9",
+            color: "#063251",
+            letterSpacing: "0.1rem",
+            px: 2,
+          }}
+        >
+          Generate
+        </Button>
+      </Box>
+    </>
+  );
 }
 
 export default App;
