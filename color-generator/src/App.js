d84e4f56b1820e58e61d93ca2baf61a7643be865
Rushikesh <pitalerushikesh@gmail.com>
2022-03-28 17:47:22 +0530

    final1

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -1,41 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { Box, Button, FormLabel, TextField } from "@mui/material";
+import { Box, Button, FormLabel, Grid, TextField } from "@mui/material";
+import Values from "values.js";
+import ColorCard from "./components/ColorCard";
 
 function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#f15025").all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let newColor = new Values(color).all(10);
+      setList(newColor);
+      setError(false);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
   return (
-    <>
-      <Box display="flex" justifyContent="space-evenly">
-        <FormLabel
+    <Grid container>
+      <Grid
+        item
+        lg={12}
+        justifyContent="start"
+        alignItems="center"
+        display="flex"
+      >
+        <Box
+          display="flex"
+          justifyContent="space-evenly"
           sx={{
-            fontSize: "1.25rem",
-            color: "#063251",
-            letterSpacing: "0.1rem",
+            my: 3,
+            ml: 3,
           }}
         >
-          Color Generator
-        </FormLabel>
-        <TextField
-          sx={{
-            mx: 2,
-          }}
-          type="number"
-          size="small"
-          autoComplete="off"
-          placeholder="#f15025"
-        />
-        <Button
-          sx={{
-            backgroundColor: "#49a6e9",
-            color: "#063251",
-            letterSpacing: "0.1rem",
-            px: 2,
-          }}
-        >
-          Generate
-        </Button>
-      </Box>
-    </>
+          <FormLabel
+            sx={{
+              fontSize: "1.25rem",
+              color: "#063251",
+              letterSpacing: "0.1rem",
+            }}
+          >
+            Color Generator
+          </FormLabel>
+          <TextField
+            sx={{
+              mx: 2,
+              borderRadius: "5%",
+              border: error ? "1px solid red" : null,
+            }}
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            size="small"
+            autoComplete="off"
+            placeholder="#f15025"
+          />
+          <Button
+            sx={{
+              backgroundColor: "#49a6e9",
+              color: "#063251",
+              letterSpacing: "0.1rem",
+              px: 2,
+            }}
+            onClick={handleSubmit}
+          >
+            Generate
+          </Button>
+        </Box>
+      </Grid>
+
+      {list.map((item, index) => {
+        return (
+          <Grid item lg={2}>
+            <ColorCard
+              key={index}
+              {...item}
+              index={index}
+              hexColor={item.hex}
+            />
+          </Grid>
+        );
+      })}
+    </Grid>
   );
 }
 
--- color-generator/src/components/ColorCard.js
+++ color-generator/src/components/ColorCard.js
@@ -1,7 +1,58 @@
-import React from "react";
+import { Box, CardContent, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
 
-const ColorCard = () => {
-  return <div>ColorCard</div>;
+const ColorCard = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+
+  const hexValue = `#${hexColor}`;
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: `rgb(${bcg})`,
+        height: "280px",
+        cursor: "pointer",
+      }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      <CardContent>
+        <Typography
+          sx={{
+            color: index > 10 ? "#fff" : "#000",
+          }}
+        >
+          {weight}%
+        </Typography>
+        <Typography
+          sx={{
+            color: index > 10 ? "#fff" : "#000",
+          }}
+        >
+          {hexValue}
+        </Typography>
+        {alert && (
+          <Typography
+            sx={{
+              color: index > 10 ? "#fff" : "#000",
+            }}
+          >
+            COPIED TO CLIPBOARD
+          </Typography>
+        )}
+      </CardContent>
+    </Box>
+  );
 };
 
 export default ColorCard;
